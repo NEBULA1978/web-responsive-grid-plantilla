effc13432cde8254b16ff292f763814a8a0fd942
NEBULA1978 <rpascualey@gmail.com>
2023-09-23 14:44:06 +0200

    Creo archivos y carpetas con bash

---
+++ archivos-carpetas.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Crear carpetas
+mkdir css img js partials
+
+# Crear archivos dentro de las carpetas
+touch css/colors.css css/common.css css/grid.css css/styles.css
+touch img/favorites.svg img/icon-customer-service.png img/icon.png img/icon-refund.png img/icon-secure.png img/icon-shipping.png img/logo.JPG img/my-account.svg img/newsletter.svg img/producto10.jpg img/producto11.jpg img/producto12.jpg img/producto1.jpg img/producto2.jpg img/producto3.jpg img/producto4.jpg img/producto5.jpg img/producto6.jpg img/producto7.jpg img/producto8.jpg img/producto9.jpg img/shopping-cart.svg
+touch js/include-html.js
+touch partials/aside.html partials/footer.html partials/menu.html partials/products.html
+touch ejemplo-basico-html.txt index.html temporalcss.txt
+
+echo "Estructura de carpetas y archivos creada exitosamente."
